e2b4d467064bf3fa2b8465175b73e5debfbf6147
Kyle Luck <luck.kyle@gmail.com>
2016-05-26 14:48:25 -0400

    fixing bug in calculator after refactor

--- Week4/05-26/calculator-redo-native-dom/calculator.js
+++ Week4/05-26/calculator-redo-native-dom/calculator.js
@@ -22,60 +22,45 @@ window.onload = function() {
 
   var display = document.getElementById('display');
 
-  var numberKeys = document.getElementsByClassName('number');
-  for (var i = 0; i < numberKeys.length; i++) {
-    numberKeys[i].addEventListener('click', function(event) {
+  document.body.addEventListener('click', function(event) {
+    var target = event.target;
+    if (target.classList.contains('number')) {
       var target = event.target;
       var number = target.textContent;
       var currentInput = display.value;
       display.value = currentInput + number;
-    });
-  }
-
-  var clearButton = document.querySelector('.clear');
-  clearButton.addEventListener('click', function() {
-    display.value = '';
-  });
-
-  var operatorButtons = document.getElementsByClassName('operator');
-  for (var j = 0; j < operatorButtons.length; j++) {
-    operatorButtons[j].addEventListener('click', function(event) {
+    }
+    else if (target.classList.contains('clear')) {
+      display.value = '';
+    }
+    else if (target.classList.contains('operator')) {
       var target = event.target;
       var newNumber = Number(display.value);
       if (operator && currentNumber) {
-
+        currentNumber = calculate(newNumber);
       }
       else {
         currentNumber = newNumber;
       }
       display.value = '';
       operator = target.textContent;
-    });
-  }
-
-  var evaluateButton = document.querySelector('.evaluate');
-  evaluateButton.addEventListener('click', function() {
-    var newNumber = Number(display.value);
-    if (operator && currentNumber) {
-      currentNumber = calculate(newNumber);
-      display.value = currentNumber;
     }
-    else {
-      display.value = '';
+    else if (target.classList.contains('evaluate')) {
+      var newNumber = Number(display.value);
+      if (operator && currentNumber) {
+        currentNumber = calculate(newNumber);
+        display.value = currentNumber;
+      }
+      else {
+        display.value = '';
+      }
+      operator = null;
     }
-    operator = null;
+    else if (target.classList.contains('decimal')) {
+      var currentInput = Number(display.value);
+      var displayWithDecimal = currentInput + ".";
+      display.value = displayWithDecimal;
+    }
   });
-
-  var decimalButton = document.querySelector('.decimal');
-  decimalButton.addEventListener('click', function() {
-    var currentInput = Number(display.value);
-    var displayWithDecimal = currentInput + ".";
-    display.value = displayWithDecimal;
-  });
-
-  // $('.decimal').click(function() {
-  //   var currentInput = Number(display.val());
-  //   var displayWithDecimal = currentInput + ".";
-  //   display.val(displayWithDecimal);
-  // });
+  //currentNumber = calculate(newNumber);
 };
