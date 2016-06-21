87647e41b6049625bb29ab434dcd741e67922eb2
Kyle Luck <luck.kyle@gmail.com>
2016-06-21 15:56:47 -0400

    improvements to getWeather file

--- Week8/06-21/nodejs/network-request.js
+++ Week8/06-21/nodejs/network-request.js
@@ -5,15 +5,33 @@ get weather and temperature for a city passed in from the CLI
 var request = require('request');
 var city = process.argv[2];
 
-request({
-  url: 'http://api.openweathermap.org/data/2.5/weather',
-  qs: {
-    q: city,
-    units: 'imperial',
-    APPID: 'eac2948bfca65b78a8c5564ecf91d00e'
+getWeather(city, function(err, data) {
+  if (err) {
+    return console.err('Get weather failed: ' + err.message);
   }
-}, function(err, response, body) {
-  var data = JSON.parse(body);
   console.log('Weather: ' + data.weather[0].description);
   console.log('Temperature: ' + data.main.temp);
 });
+
+function getWeather(city, callback) {
+  request({
+    url: 'http://api.openweathermap.org/data/2.5/weather',
+    qs: {
+      q: city,
+      units: 'imperial',
+      APPID: 'eac2948bfca65b78a8c5564ecf91d00e'
+    }
+  }, function(err, response, body) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    try {
+      var data = JSON.parse(body);
+      callback(null, data);
+    } catch (parseError) {
+      callback(parseError);
+      return;
+    }
+  });
+}
