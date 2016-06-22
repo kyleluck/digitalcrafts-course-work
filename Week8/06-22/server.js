e72058d0507723f8a652066d3704168df1dff6bf
Kyle Luck <luck.kyle@gmail.com>
2016-06-22 14:02:45 -0400

    node server

--- Week8/06-22/server.js
+++ Week8/06-22/server.js
@@ -4,10 +4,46 @@
 Make a server that returns "Hello, world!" to the web browser.
 */
 var http = require('http');
+var url = require('url');
+
 const PORT = 3000;
 
+var title = 'Node Says Hello';
+var html = `
+<html>
+  <head>
+    <title>${title}</title>
+    <script src='script.js'></script>
+    <link rel='stylesheet' href='style.css'>
+  </head>
+  <body>
+    <h1>Hello Kyle. ${title}</h1>
+    <h5>Node is freaking awesome</h5>
+  </body>
+</html>`;
+
+var css = `
+body {
+  background: yellow;
+}`;
+
+var js = `
+alert('Hahahahaha');
+`;
+
 var server = http.createServer(function(request, response) {
-  response.write('Hello World!');
+  var url = request.url;
+  if (url === '/') {
+    response.write(html);
+  } else if (url === '/script.js') {
+    response.write(js);
+  } else if (url === '/style.css') {
+    response.write(css);
+  }
+  
+  //var queryObject = url.parse(request.url, true).query;
+  //console.log(queryObject);
+
   response.end();
 });
 
