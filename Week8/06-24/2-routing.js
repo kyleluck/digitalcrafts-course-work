ebd2fed935f5de5ddc47903614ec0f97db8a92d3
Kyle Luck <luck.kyle@gmail.com>
2016-06-24 09:41:42 -0400

    adding hbs module

--- Week8/06-24/2-routing.js
+++ Week8/06-24/2-routing.js
@@ -9,6 +9,11 @@ app.get('/about', function(request, response) {
   response.send('This is just an example app demonstrating simple routing in express.');
 });
 
+/*
+Nested Example:
+app.get('/organization/:orgId/client/:clientId/project/:projectId'...)
+*/
+
 app.get('/:pageName', function(request, response) {
   var pageName = request.params.pageName;
   response.send('Hi, I will tell you everything I know about ' + pageName + '.');
