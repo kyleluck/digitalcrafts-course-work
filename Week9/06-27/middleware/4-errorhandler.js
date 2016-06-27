21222ec1d71bb8464cea62d8ac74ad909113938a
Kyle Luck <luck.kyle@gmail.com>
2016-06-27 14:27:02 -0400

    middleware exercises

---
+++ Week9/06-27/middleware/4-errorhandler.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+// the handler functions we've been writing are in fact middlewares
+app.get('/', function(request, response, next) {
+  fs.readFile('wikipage.txt', function(err, data) {
+    if (err) {
+      next(err);
+      return;
+    }
+    response.render('page.hbs', {
+      content: data.toString()
+    });
+  });
+});
+
+/*
+An error handle will take an extra err parameter in the beginning - it has to take all 4 parameters.
+*/
+app.use(function(err, request, response, next) {
+  var errorReport = err.message + '\n' + err.stack + '\n\n';
+  fs.appendFile('error.log', errorReport);
+  next();
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000.');
+});
