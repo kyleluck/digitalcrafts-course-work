44ddb45d6c20ce9473e20c2ae0983d7b4adfaf9c
Kyle Luck <luck.kyle@gmail.com>
2016-07-12 13:45:16 -0400

    learning python

---
+++ Week11/07-12/python-the-hard-way/ex1.py
@@ -0,0 +1,12 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+#print "What's up Kyle"
+
+print "I could have code like this" # and the comment after is ignored
+# print "this won't run."
+print "This will run."
