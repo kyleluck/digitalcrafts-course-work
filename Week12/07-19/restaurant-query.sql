82be32051a56ec8ec229ee251c9ab16a39c6f0aa
Kyle Luck <luck.kyle@gmail.com>
2016-07-19 15:37:44 -0400

    restaurant query change

--- Week12/07-19/restaurant-query.sql
+++ Week12/07-19/restaurant-query.sql
@@ -1,9 +1,28 @@
-#query to then display author, review title, full review, stars, restaurant name:
+-- query to  display author, review title, full review, stars, restaurant name:
+
 select
-  users.name, reviews.title, reviews.review, reviews.stars, restaurants.name
+  users.name as author,
+  reviews.title as title,
+  reviews.review as review,
+  reviews.stars as stars,
+  restaurants.name as restaurant
 from
   users
 inner join
   reviews on users.id = reviews.author
 inner join
   restaurants on reviews.restaurant_id = restaurants.id;
+
+-- OR
+
+select
+  users.name as author,
+  reviews.title as title,
+  reviews.review as review,
+  reviews.stars as stars,
+  restaurants.name as restaurant
+from
+  users, reviews, restaurants
+where
+  users.id = reviews.author and
+  reviews.restaurant_id = restaurants.id;
