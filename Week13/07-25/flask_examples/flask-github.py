9d01d4083c82430b3e606dfecd2f784a45329d48
Kyle Luck <luck.kyle@gmail.com>
2016-07-25 14:18:24 -0400

    finishing github flask exercise

--- Week13/07-25/flask_examples/flask-github.py
+++ Week13/07-25/flask_examples/flask-github.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import pg
 
 db = pg.DB(dbname='github')
@@ -7,14 +7,43 @@ app = Flask('MyApp')
 
 @app.route('/')
 def display_form():
-    # get possible user names from db for dropdown
-    users = db.query("select id, fullname from user_table order by username asc")
-    projects = db.query("select name from repo order by name asc")
+    # only display if user is logged in
+    if 'name' in session:
+        # get possible user names from db for dropdown
+        users = db.query("select id, fullname from user_table order by username asc")
 
-    return render_template('github-insert-form.html',
-            title="GitHub",
-            users = users.namedresult(),
-            projects = projects.namedresult())
+        # get projects for user logged in:
+        username = session['name']
+        useridquery = db.query("select id from user_table where username = '%s' limit 1" % username)
+        user = useridquery.namedresult()
+
+        projects = db.query("select name from repo where user_id = '%s' order by name asc" % user[0].id)
+
+        return render_template('github-insert-form.html',
+                title="GitHub",
+                users = users.namedresult(),
+                projects = projects.namedresult())
+    else:
+        return redirect('/login')
+
+@app.route('/login')
+def login_form():
+    # remove session variable name if exists. user is logging out
+    if 'name' in session:
+        del session['name']
+
+    return render_template('github-login.html')
+
+@app.route('/submit-login', methods=['POST'])
+def handle_login():
+    username = request.form['username']
+    # check to see if username is in db
+    check = db.query("select * from user_table where username = '%s' LIMIT 1" % username)
+    if check.namedresult():
+        session['name'] = username
+        return redirect('/')
+    else:
+        return redirect('/login')
 
 @app.route('/save', methods=['POST'])
 def save_project():
@@ -43,6 +72,9 @@ def find_user_knows():
     result = db.query(find_query)
     return render_template('user-knows.html',
         techs = result.namedresult())
+
+# Secret key for session
+app.secret_key = 'CSF686CCF85C6FRTCHQDBJDXHBHC1G478C86GCFTDCR'
 
 if __name__ == "__main__":
     app.run(debug=True)
